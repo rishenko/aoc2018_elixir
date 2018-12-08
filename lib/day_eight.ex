fba955cbf402262d72d57935b434b68829ba1e74
Kevin McAbee <kevin.mcabee@gmail.com>
2018-12-08 15:58:38 -0500

    quick cleanup

--- lib/day_eight.ex
+++ lib/day_eight.ex
@@ -3,29 +3,25 @@ defmodule Aoc2018.DayEight do
   def records_to_tree([]), do: :end_of_license
 
   def records_to_tree([num_nodes, num_metadata_entries | rest_license]) do
-    if num_nodes == 0 do
-      {metadata_entries, rest_license} = Enum.split(rest_license, num_metadata_entries)
-      metadata_sum = calc_metadata_entries(metadata_entries)
-      {{0, num_metadata_entries, metadata_entries, []}, metadata_sum, rest_license}
-    else
-      {children, child_metadata_sum, rest_license} =
-        Enum.reduce(
-          1..num_nodes,
-          {[], 0, rest_license},
-          fn _, {children, metadata_sum, license} ->
-            case records_to_tree(license) do
-              :end_of_license ->
-                {children, metadata_sum, license}
+    {children, child_metadata_sum, rest_license} =
+      calc_child_records(num_nodes, rest_license)
 
-              {record, child_metadata_sum, license} ->
-                {[record | children], metadata_sum + child_metadata_sum, license}
-            end
-          end
-        )
-      {metadata_entries, rest_license} = Enum.split(rest_license, num_metadata_entries)
-      metadata_sum = calc_metadata_entries(metadata_entries) + child_metadata_sum
-      {{num_nodes, num_metadata_entries, metadata_entries, children}, metadata_sum, rest_license}
-    end
+    {metadata_entries, rest_license} = Enum.split(rest_license, num_metadata_entries)
+    metadata_sum = calc_metadata_entries(metadata_entries) + child_metadata_sum
+    {{num_nodes, num_metadata_entries, metadata_entries, children}, metadata_sum, rest_license}
+  end
+
+  def calc_child_records(0, rest_license), do: {[], 0, rest_license}
+
+  def calc_child_records(num_nodes, rest_license) do
+    Enum.reduce(
+      1..num_nodes,
+      {[], 0, rest_license},
+      fn _, {children, metadata_sum, license} ->
+        {record, child_metadata_sum, license} = records_to_tree(license)
+        {[record | children], metadata_sum + child_metadata_sum, license}
+      end
+    )
   end
 
   def calc_metadata_entries(entries) do
